cb35e97c99446e179e03f241a2e80aa72df116f0
Alvaro234 <alvaroglezvilla@gmail.com>
2024-01-27 17:15:19 +0100

    crop fix

--- data/test.py
+++ data/test.py
@@ -1,19 +1,26 @@
 import rasterio
 import matplotlib.pyplot as plt
-# Replace with the actual path to your Sentinel-2 image
+import PIL as PIL
+# Replace with the actual paths to your Sentinel-2 images
 image_path = "data/cropped/cropped_2023_B04_20m.jp2"
-with rasterio.open(image_path) as src:
-    # Get the coordinates of the top-left and bottom-right corners
-    top_left = (src.bounds.left, src.bounds.top)
-    bottom_right = (src.bounds.right, src.bounds.bottom)
+image_path1 = "data/cropped/cropped_2024_B04_20m.jp2"
+image_path2 = "data/cropped/cropped_2023_B8A_20m.jp2"
+image_path3 = "data/cropped/cropped_2023_B8A_20m.jp2"
 
-print("Top-left corner coordinates:", top_left)
-print("Bottom-right corner coordinates:", bottom_right)
-
-def display_image(image_path):
+def display_image(ax, image_path, title):
     with rasterio.open(image_path) as src:
-        plt.imshow(src.read(1), cmap='viridis')
-        plt.title("Cropped Image")
-        #plt.colorbar()
-        plt.show()
-display_image(image_path)
+        ax.imshow(src.read(1), cmap='gray')
+        ax.set_title(title)
+        ax.axis('off')
+        
+# Create a 2x2 subplot layout
+fig, axs = plt.subplots(2, 2, figsize=(10, 10))
+
+# Display each image in a subplot
+display_image(axs[0, 0], image_path, "Image 1")
+display_image(axs[0, 1], image_path1, "Image 2")
+display_image(axs[1, 0], image_path2, "Image 3")
+display_image(axs[1, 1], image_path3, "Image 4")
+
+plt.tight_layout()
+plt.show()
