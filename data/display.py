534ba652d6f9f627d18b28c30a394a12582cdfd4
Alvaro234 <alvaroglezvilla@gmail.com>
2024-02-12 10:43:08 +0100

    qwasd

--- data/display.py
+++ data/display.py
@@ -1,28 +1,29 @@
 import rasterio
+import numpy as np
 import matplotlib.pyplot as plt
-import PIL as PIL
-# Replace with the actual paths to your Sentinel-2 images
-image_path = "data/combined_output/2024_combined.tif"
-image_path1 = "data/cropped_combined/cropped_T29SQA_20240121T111259_B02_10m.jp2"
-image_path2 = "data/cropped_combined/cropped_T29SQA_20240121T111259_B03_10m.jp2"
-image_path3 = "data/cropped_combined/cropped_T29SQA_20240121T111259_B08_10m.jp2"
 
-
-
-def display_image(ax, image_path, title):
+def display_rgb_image(image_path):
     with rasterio.open(image_path) as src:
-        ax.imshow(src.read(1))
-        ax.set_title(title)
-        ax.axis('off')
-        
-# Create a 2x2 subplot layout
-fig, axs = plt.subplots(2, 2, figsize=(10, 10))
+        # Read the RGB image data
+        rgb_image = src.read()
 
-# Display each image in a subplot
-display_image(axs[0, 0], image_path, "Image 1")
-display_image(axs[0, 1], image_path1, "Image 2")
-display_image(axs[1, 0], image_path2, "Image 3")
-display_image(axs[1, 1], image_path3, "Image 4")
+        # Transpose the shape to (channels, height, width)
+        rgb_image = np.transpose(rgb_image, (1, 2, 0))
 
-plt.tight_layout()
-plt.show()
+        # Ensure the data type is float
+        rgb_image = rgb_image.astype(np.float32)
+
+        # Normalize the image to [0, 1] range
+        rgb_image = rgb_image / 65535.0
+
+        # Ensure the color channels are in the correct order (RGB)
+        rgb_image = rgb_image[:, :, [2, 1, 0]]
+
+        # Display the RGB image
+        plt.imshow(rgb_image)
+        plt.title("Combined RGB Image")
+        plt.axis('off')  # Turn off axis
+        plt.show()
+
+
+display_rgb_image("data/combined_output/2021_combined_rgb.tif")
