e782566a52e7b7dbcad912e0041404e02b6d7371
Alvaro234 <alvaroglezvilla@gmail.com>
2024-02-27 17:28:02 +0100

    ndvi tests

--- data/data_generator.py
+++ data/data_generator.py
@@ -5,10 +5,10 @@ import earthpy.plot as ep
 import imageio
 
 # Load the individual channel images (red, green, blue, yellow)
-red_channel = rasterio.open('data\cropped_bands\cropped_2021_B04_10m.jp2').read(1)
-green_channel = rasterio.open('data\cropped_bands\cropped_2021_B03_10m.jp2').read(1)
-blue_channel = rasterio.open('data\cropped_bands\cropped_2021_B02_10m.jp2').read(1)
-nir_channel = rasterio.open('data\cropped_bands\cropped_2021_B08_10m.jp2').read(1)
+red_channel = rasterio.open('data\cropped_bands\cropped_2024_B04_10m.jp2').read(1)
+green_channel = rasterio.open('data\cropped_bands\cropped_2024_B03_10m.jp2').read(1)
+blue_channel = rasterio.open('data\cropped_bands\cropped_2024_B02_10m.jp2').read(1)
+nir_channel = rasterio.open('data\cropped_bands\cropped_2024_B08_10m.jp2').read(1)
 
 # Normalize each channel to [0, 255]
 def normalize_channel(channel):
@@ -57,10 +57,25 @@ NDVI = ndvi_calc(red_channel,nir_channel)
 print(red_channel[100,100])
 print(nir_channel[100,100])
 print((NDVI)[100,100])
-#ep.plot_bands(NDVI, cmap = 'RdYlGn', title = 'NDVI map')
+ep.plot_bands(NDVI, cmap = 'RdYlGn', title = 'NDVI map')
 water = NDVI < 0
-#ep.plot_bands(water, cmap = 'Blues', title = 'NDVI map')
+ep.plot_bands(water, cmap = 'Blues', title = 'NDVI map')
 
-# Save the NDVI array directly as a PNG file
-imageio.imwrite("data/ndvi/ndvi_2021.png", NDVI)
 
+# Define output file path
+output_ndvi_file = 'data/ndvi/2024_ndvi.tif'
+
+# Define metadata
+metadata = {
+    'driver': 'GTiff',
+    'dtype': 'float32',  # Adjust data type as needed
+    'count': 1,  # Number of bands
+    'height': NDVI.shape[0],  # Height of the array
+    'width': NDVI.shape[1],  # Width of the array
+}
+
+# Write the NDVI array to a new TIFF file
+with rasterio.open(output_ndvi_file, 'w', **metadata) as dst:
+    dst.write(NDVI, 1)  # Assuming NDVI is stored in the first band
+
+print(f"NDVI image saved to {output_ndvi_file}")
